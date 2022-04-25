43f8c0e85c2cf52f19e296069ade1f90f59e99e6
Mxrk122 <60331995+Mxrk122@users.noreply.github.com>
2022-04-25 14:51:07 -0600

    Configuracion inicial del proyecto -> webpack, babel y sass

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../Assets/Styles/app.scss"
+
+function App() {
+
+    const suma = 5 + 6
+
+  return (
+    <div className = 'app'>
+        <h1>{suma}</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const HTMlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  plugins: [
+    new HTMlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/react"],
+          },
+        },
+      },
+
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
